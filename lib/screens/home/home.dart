f6cfa6feb0bf30dabc4ef1f9cd0b82f9f715304a
Shubham Kumar <arjunraj582shu@gmail.com>
2023-09-28 16:26:51 +0530

    trying to get the list of prefrencerce from the users

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,29 +1,35 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/brew.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: const Text("Brew Crew"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          TextButton(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              child: Row(
-                children: [
-                  Icon(Icons.person),
-                  Text("logout"),
-                ],
-              ))
-        ],
+    return StreamProvider<List<Brew>>.value(
+        value: DatabaseService.withoutUID().brews, initialData: [])(
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: const Text("Brew Crew"),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            TextButton(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                child: Row(
+                  children: [
+                    Icon(Icons.person),
+                    Text("logout"),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,10 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
   final String uid;
   DatabaseService({required this.uid});
+  DatabaseService.withoutUID() : uid = "";
   //collection reference
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection("brews");
@@ -13,5 +15,21 @@ class DatabaseService {
       'name': name,
       'strength': strength,
     });
+  }
+
+  // brew list from screenshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      //print(doc.data);
+      return Brew(
+          name: doc['name'] ?? '',
+          strength: doc['strength'] ?? 0,
+          sugars: doc['sugars'] ?? '0');
+    }).toList();
+  }
+
+  //get brew stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
