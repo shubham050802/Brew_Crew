edb594019acb207d81ba7f9676132a358cd882e7
Shubham Kumar <arjunraj582shu@gmail.com>
2023-09-28 17:23:33 +0530

    Added body widgets
    these are the containers that have the code for body of the home screen.

---
+++ lib/screens/home/brew_list.dart
@@ -0,0 +1,27 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BrewList extends StatefulWidget {
+  @override
+  State<BrewList> createState() => BrewListState();
+}
+
+class BrewListState extends State<BrewList> {
+  @override
+  Widget build(BuildContext context) {
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.strength);
+      print(brew.sugars);
+    });
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
+  }
+}
